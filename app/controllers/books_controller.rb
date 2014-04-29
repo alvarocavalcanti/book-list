fdce2adcbee71291cbad3c347dbc8a88efba703e
Alvaro Cavalcanti <alvarovictor@gmail.com>
2014-04-29 14:19:24 -0300

    URL validadtion

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,15 @@
 class BooksController < ApplicationController
+	# http://bit.ly/1hPYNko
 	def new
 		if !params.has_key?(:url) || params[:url].blank?
 			flash[:danger] = "Please provdie a URL."
 			redirect_to home_path
 		end
 		@book = Book.new(search_params)
-		@book.save
+		if !@book.save
+			flash[:danger] = "Invalid URL"
+			redirect_to home_path
+		end
 	end
 
 	private 
