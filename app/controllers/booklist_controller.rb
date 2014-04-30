8a3268d5e32c46f42fa1010a81c36c59dfa7713f
Alvaro Cavalcanti <alvarovictor@gmail.com>
2014-04-30 17:17:14 -0300

    Books: Listing

--- app/controllers/booklist_controller.rb
+++ app/controllers/booklist_controller.rb
@@ -1,4 +1,5 @@
 class BooklistController < ApplicationController
 	def index
+		@books = Book.all
 	end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,8 @@ class SessionsController < ApplicationController
 		user = User.find_by(email: params[:session][:email].downcase)
 		if user && user.authenticate(params[:session][:password])
 			# Sign the user in and redirect to the user's show page.
-			session[:username] = user.username;
+			session[:username] = user.username
+			session[:userid] = user.id
 
 			redirect_to home_path
 		else
