1be98d4331359e7cfc2df44d51fb4257c063fd8b
Alvaro Cavalcanti <alvarovictor@gmail.com>
2014-05-13 15:20:40 -0300

    Models unit tests

--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -4,4 +4,33 @@ class BookTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  setup do
+  	@new_book = Book.new
+  	@new_book.url = "http://www.url.com"
+  	@new_book.title = "A Random Title"
+	@new_book.price_initial = 0.0
+	@new_book.price_current = 0.0
+	@new_book.isbn = "01010101010"
+	@new_book.author = "John Doe"
+	@new_book.bought = false
+	@new_book.read = false
+  end
+
+  test "should not save an empty book" do
+  	book = Book.new
+  	assert_not book.save, "Saved an empty book"
+  end
+
+  test "should not save a book with an existing URL" do
+  	@new_book.url = books(:monsterhigh).url
+
+  	assert_not @new_book.save, "Saved a book with an existing URL"
+  end
+
+  test "should not save a book with an existing ISBN" do
+  	@new_book.isbn = books(:monsterhigh).isbn
+
+  	assert_not @new_book.save, "Saved a book with an existing ISBN"
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,29 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  setup do
+  	@new_user = User.new
+  	@new_user.username = "myusername"
+  	@new_user.email = "my@email.com"
+  	@new_user.password = "123456"
+  end
+
+  test "should not create an empty User" do
+  	user = User.new
+
+  	assert_not user.save, "Saved an empty User"
+  end
+
+  test "should not create an user with an existing email" do
+  	@new_user.email = users(:alvarogmail).email
+
+  	assert_not @new_user.save, "Saved a User with an existing email"
+  end
+
+  test "should not create an user with an existing username" do
+  	@new_user.username = users(:alvarogmail).username
+
+  	assert_not @new_user.save, "Saved a User with an existing username"
+  end
 end
